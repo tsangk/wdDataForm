b78a89906772912b9df35871dd4a695564dfe727
unknown <kenneth.tsang@7M8BFS1.schools.mn>
2012-10-31 00:14:19 +1100

    new layout

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
-<html>
-	<head>
-		<!-- Copyright (c) Kenneth Tsang 2012 -->
-		<!-- Automated access to this program is strictly forbidden -->
-		<title>Wikidot Data Form (Forgiving to Strict)</title>
-		<style>body{ font-family: verdana, arial, helvetica, sans-serif; font-size: 12px; }td{ vertical-align: top; font-size: 12px; }#status{text-align: center;padding: 0 1em;margin: 0.5em 0;border: 1px solid #EAA;}</style>
-	</head>
-	<body>
-		<h1>Wikidot Data Form Converter</h1>
-		<p>This program converts existing forgiving YAML code into the current strict version.  Please note that this program is not perfect and may have bugs.  If you encounter any issues, please leave a comment <a href="http://jxeeno.tk/blog:wikidot:from-forgiving-to-strict">here</a> with the exact code that's causing the issue.</p>
-		<form method="post">
-			<table style="width: 100%;">
-				<tbody>
 <?php
 	$textarea = "";
 	function checkStatus($array){
@@ -49,46 +36,97 @@
 		$status = "<b>Converter Ready!</b>";
 	}
 ?>
-				<tr>
-					<td colspan=3>
-						<div id="status"><?php echo $status; ?></div>
-					</td>
-				</tr>
-				<tr>
-					<td style="width: 45%;">
-						<b>Input:</b> Paste the entire _template page below (copy from <i>view source</i> or <i>Edit</i> textarea)
-					</td>
-					<td style="width: 10%;"></td>
-					<td style="width: 45%;">
-						<b>Output:</b> Your new compliant _template code:
-					</td>
-				</tr>
-				<tr>
-					<td>
-							<textarea name="yaml" style="height: 300px; width: 100%;"><?php if(!empty($_POST["yaml"])){ echo htmlentities($_POST["yaml"]); } ?></textarea><br />
-					</td>
-					<td style="vertical-align:middle;text-align:center;"><input type="submit" value=">>" /></td>
-					<td>
-						<textarea style="height: 300px; width: 100%;"><?php echo $textarea; ?></textarea>
-					</td>
-				</tr></tbody>
-			</table>
-		</form>
-		<hr />
-		<center>
-			<script type="text/javascript"><!--
-			google_ad_client = "ca-pub-2102065243904072";
-			/* Ad Bottom */
-			google_ad_slot = "2376964219";
-			google_ad_width = 728;
-			google_ad_height = 90;
-			//-->
-			</script>
-			<script type="text/javascript"
-			src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-			</script>
-		</center>
-		<hr />
-		&copy; Kenneth Tsang 2012.  All rights reserved.  Powered by SPYC and syYaml.
+<html>
+	<head>
+		<!-- Copyright (c) Kenneth Tsang 2012 -->
+		<!-- Automated access to this program is strictly forbidden -->
+		<title>Wikidot Data Form (Forgiving to Strict)</title>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+		<style>
+			html{
+				font-family: 'Open Sans', sans-serif;
+				font-size: 12px;
+			}
+			td{ vertical-align: top; font-size: 12px; }
+			#status{text-align: center;padding: 0 1em;margin: 0.5em 0;border: 1px solid #EAA;}
+			#header{
+				background: #3b3b3b url(img/texture.png) repeat;
+				height: 30px;
+				width: 100%;
+				position: fixed;
+				top: 0px;
+				left: 0px;
+				color: white;
+				vertical-align: middle;
+				line-height: 32px;
+				padding-left: 10px;
+				padding-top: 3px;
+				padding-bottom: 3px;
+				white-space: nowrap;
+				z-index: 9;
+				border-bottom: 1px solid #3b3b3b;
+				-webkit-box-shadow: 0 0 2px rgba(0, 0, 0, .52);
+				text-align: center;
+			}
+			#header #logo{
+				font-weight: bold;
+				color: white;
+				text-decoration: none;
+				font-size: 120%;
+			}
+			#content{
+				margin-top: 50px;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="header"><span id="logo">Wikidot Data Form Converter</span></div>
+		<div id="content">
+			<p>This program converts existing forgiving YAML code into the current strict version.  Please note that this program is not perfect and may have bugs.  If you encounter any issues, please leave a comment <a href="http://jxeeno.tk/blog:wikidot:from-forgiving-to-strict">here</a> with the exact code that's causing the issue.</p>
+			<form method="post">
+				<table style="width: 100%;">
+					<tbody>
+					<tr>
+						<td colspan=3>
+							<div id="status"><?php echo $status; ?></div>
+						</td>
+					</tr>
+					<tr>
+						<td style="width: 45%;">
+							<b>Input:</b> Paste the entire _template page below (copy from <i>view source</i> or <i>Edit</i> textarea)
+						</td>
+						<td style="width: 10%;"></td>
+						<td style="width: 45%;">
+							<b>Output:</b> Your new compliant _template code:
+						</td>
+					</tr>
+					<tr>
+						<td>
+								<textarea name="yaml" style="height: 300px; width: 100%;"><?php if(!empty($_POST["yaml"])){ echo htmlentities($_POST["yaml"]); } ?></textarea><br />
+						</td>
+						<td style="vertical-align:middle;text-align:center;"><input type="submit" value=">>" /></td>
+						<td>
+							<textarea style="height: 300px; width: 100%;"><?php echo $textarea; ?></textarea>
+						</td>
+					</tr></tbody>
+				</table>
+			</form>
+			<hr />
+			<center>
+				<script type="text/javascript"><!--
+				google_ad_client = "ca-pub-2102065243904072";
+				/* Ad Bottom */
+				google_ad_slot = "2376964219";
+				google_ad_width = 728;
+				google_ad_height = 90;
+				//-->
+				</script>
+				<script type="text/javascript"
+				src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+				</script>
+			</center>
+			<hr />
+			&copy; Kenneth Tsang 2012.  All rights reserved.  Powered by SPYC and syYaml.
+		</div>
 	</body>
 </html>
