58fb64bd44bed3555bec8d7b322690300b5262b0
unknown <kenneth.tsang@7M8BFS1.schools.mn>
2012-11-01 23:30:28 +1100

    Moved to ajax version

--- index.php
+++ index.php
@@ -1,68 +1,10 @@
-<?php
-	$templateCode = html_entity_decode($_POST["yaml"]);
-
-	$textarea = "";
-	function checkStatus($array){
-		$acceptedValues = array("label","type","values","default","category","join","before","after","width","height","match","match-error","hint","value","default-schema","required");
-		if(isset($array["fields"])){
-			if(count($array)!=1){
-				return "<b><span class=\"er-red\">Notice! There is more than one parent-level value. Usually, it is reserved for <i>fields:</i> only.</span></b><br />Please check indenting and copy the <b>entire</b> _template from the page editor.";
-			}else{
-				if(!is_array($array['fields'])){
-					return "<b><span class=\"er-red\">Notice! There are no fields defined.</span></b><br />Please check indenting and copy the <b>entire</b> _template from the page editor.";
-				}else{
-					$isokay = true;
-					$noticeString = "";
-					foreach($array["fields"] as $key=>$value){
-						foreach($value as $k => $v){
-							if(!in_array($k,$acceptedValues)){
-								$isokay = false;
-								$noticeString .= "<br />Please check property <i>$k</i> under field <i>$key</i>";
-							}
-						}
-					}
-					if(!$isokay){
-						return "<b><span class=\"er-red\">Notice! There are invalid data form properties defined.</span></b>$noticeString";
-					}else{
-						return "<b>Your data form has been successfully converted!</b>";
-					}
-				}
-			}
-		}else{
-			return "<b><span class=\"er-red\">ERROR! Invalid Data Form input!  YAML must contain <i>fields:</i></span></b><br />Please check if you have spelt <i>fields:</i> correctly!";
-		}
-	}
-	if(!empty($templateCode)){
-		require_once('spyc.php');
-		require_once('sfYamlDumper.php');
-		if(preg_match("/\[\[form\]\]([\s\S]+)\[\[\/form\]\]/",$templateCode,$matches)){
-			$dumper = new sfYamlDumper();
-
-			// Bug fixes (reported by Ed Johnson)
-			// Colon in line
-			$matches[1] = preg_replace("/([\n\r] +)([^:]+):( ?)([^\"'\n\r][^\n\r]+:[^\n\r]+[^\"'\n\r])($|\n|\r)/","$1$2:$3'$4'$5",$matches[1]);
-
-			// Bracket at end of line
-			$matches[1] = preg_replace("/([\n\r] +)([^:]+):( ?)(\[+)($|\n|\r)/","$1$2:$3'$4'$5",$matches[1]);
-			$dfarray = Spyc::YAMLLoad(trim($matches[1]));
-			$status = checkStatus($dfarray);
-			$textarea = preg_replace("/\[\[form\]\]([\s\S]+)\[\[\/form\]\]/","�DFREPLACE�",$templateCode);
-			$textarea = str_replace("�DFREPLACE�","[[form]]\n".$dumper->dump($dfarray,999)."[[/form]]",$textarea);
-		}else{
-			$status = "<b><span class=\"er-red\">ERROR! Please enter _template page with [[form]] blocks!</span></b>";
-			$textarea = "Enter your _template page with the non-complaint data form YAML and click the convert button!";
-		}
-	}else{
-		$textarea = "Enter your _template page with the non-complaint data form YAML and click the convert button!";
-		$status = "<b>Converter Ready!</b>";
-	}
-?>
 <html>
 	<head>
 		<!-- Copyright (c) Kenneth Tsang 2012 -->
 		<!-- Automated access to this program is strictly forbidden -->
 		<title>Wikidot Data Form (Forgiving to Strict)</title>
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 		<style>
 			html{
 				font-family: 'Open Sans', sans-serif;
@@ -105,9 +47,19 @@
 				border-radius: 10px;
 			}
 			.er-red{ color: red;}
+			#spinner-wrap{ display:none; }
 		</style>
 		<script type="text/javascript">
 			function selectAll(el){el.focus();el.select();}
+			function convertYAML(){
+				$("#spinner-wrap").fadeIn();
+				$("#button-wrap").hide();
+				$.post("convert.php",$("#yamlForm").serialize(),function(data){
+					$("#outputTA").val(data.textarea);
+					$("#status").html(data.status);
+					$("#spinner-wrap").fadeOut(function(){$("#button-wrap").fadeIn();});
+				},"json");
+			}
 		</script>
 	</head>
 	<body>
@@ -134,12 +86,12 @@
 					<li>Check if there are any errors in the status box and copy the converted code and replace the existing content</li>
 				</ol>
 			</p>
-			<form method="post" class="grayshade">
+			<form method="post" action="javascript:;" class="grayshade" onsubmit="convertYAML();" id="yamlForm">
 				<table style="width: 100%; padding: 10px;">
 					<tbody>
 					<tr>
 						<td colspan=3>
-							<div id="status"><?php echo $status; ?></div>
+							<div id="status"><b>Converter Ready</b></div>
 						</td>
 					</tr>
 					<tr>
@@ -153,11 +105,11 @@
 					</tr>
 					<tr>
 						<td>
-								<textarea name="yaml" style="height: 300px; width: 100%;"><?php if(!empty($templateCode)){ echo htmlentities($templateCode,ENT_COMPAT | ENT_HTML401,"UTF-8"); } ?></textarea><br />
+								<textarea id="inputTA" name="yaml" style="height: 300px; width: 100%;"></textarea><br />
 						</td>
-						<td style="vertical-align:middle;text-align:center;"><input type="submit" value=">>" /></td>
+						<td style="vertical-align:middle;text-align:center;"><div id="button-wrap"><input type="submit" value=">>" /></div><div id="spinner-wrap"><img src="img/spin.gif" /></div></td>
 						<td>
-							<textarea id="outputTA" onclick="selectAll(this);" style="height: 300px; width: 100%;"><?php echo htmlentities($textarea,ENT_COMPAT | ENT_HTML401,"UTF-8"); ?></textarea>
+							<textarea id="outputTA" onclick="selectAll(this);" style="height: 300px; width: 100%;"></textarea>
 						</td>
 					</tr>
 					<tr>
